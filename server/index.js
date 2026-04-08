73b8f5a19d8e373a6601ca0631560c81f79c66ab
MeetPrajapati49 <4meetp@gmail.com>
2026-04-08 18:05:05 +0530

    feat: initialize Express server with Groq API integration for chat completions

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 require('dotenv').config()
 const Groq = require('groq-sdk')
 
@@ -10,7 +9,17 @@ const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY
 })
 
-app.use(cors())
+// Manual CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
